5038b3e0f33beaefbde8be1ac26c6224c4f91b6f
Sean Amoroso <sean.amoroso@connectiverx.com>
2021-06-09 12:12:22 -0400

    reproduce issue

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 #get highest tag number
 VERSION=`git describe --abbrev=0 --tags`
 
@@ -20,11 +22,13 @@ echo "Updating $VERSION to $NEW_TAG"
 
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
+echo "I'm about to describe..."
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 #only tag if no tag already
 #to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
 if [ -z "$NEEDS_TAG" ]; then
+  echo "I'm about to git config..."
   git config --local user.email "action@github.com"
   git config --local user.name "GitHub Action"
   git tag -a $NEW_TAG -m "Applying tag bump"
