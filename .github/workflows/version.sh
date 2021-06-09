6b27aaba1bdf9d9535654a34f40ab8f679b85619
Sean Amoroso <sean.amoroso@connectiverx.com>
2021-06-09 12:22:55 -0400

    do it

--- .github/workflows/version.sh
+++ .github/workflows/version.sh
@@ -19,21 +19,13 @@ VNUM4=$((VNUM4+1))
 #create new tag
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3.$VNUM4"
 
-echo "Updating $VERSION to $NEW_TAG"
-
-#get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-echo "I'm about to describe..."
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
-
-#only tag if no tag already
-#to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
-if [ -z "$NEEDS_TAG" ]; then
-  echo "I'm about to git config..."
-  git config --local user.email "action@github.com"
-  git config --local user.name "GitHub Action"
-  git tag -a $NEW_TAG -m "Applying tag bump"
-  git push origin $NEW_TAG
+# check if there's already a tag on the commit
+if `git describe --contains HEAD 2&>1 > /dev/null`; then
+    echo "Already a tag on this commit"
 else
-  echo "Already a tag on this commit"
+    echo "Updating $VERSION to $NEW_TAG"
+    git config --local user.email "action@github.com"
+    git config --local user.name "GitHub Action"
+    git tag -a $NEW_TAG -m "Applying tag bump"
+    git push origin $NEW_TAG
 fi
